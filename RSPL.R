7b557c618b0bb03d27f55f98829d11247fe3f92c
Chris Kolb <chris.kolb@hu-berlin.de>
2018-05-29 00:06:18 +0200

    test rstudio git

--- RSPL.R
+++ RSPL.R
@@ -1,11 +1,13 @@
 #Set-up
 
 rm(list=ls())
+
+#setwd(path) in path.R
 source("path.R")
-#packages und library sollten nicht in einem script geladen werden.
+
+#install and load packages
 #source("packages.R")
 source("library.R")
-
 
 #############################################################################################
 ####################  Data cleaning #########################################################
