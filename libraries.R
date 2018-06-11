433a942eec644755f46edd9cae9915f65ce58a96
konstantingoebler <goeblerk@hu-berlin.de>
2018-06-11 15:05:01 +0200

    library and packages split

---
+++ libraries.R
@@ -0,0 +1,21 @@
+
+library(rio) #reading data into R
+library(foreign) 
+library(car) # scatter3d()
+library(Hmisc) # describe(mydata)
+library(varhandle)
+library(pastecs) #stat.desc(mydata)
+library(rms)
+library(survminer)
+library(plm)
+library(data.table)
+library(bigmemory)
+library(Matrix)
+library(stats)
+library(survival)
+library(bitops)
+library(haven)
+library(plyr) #plyr muss vor dplyr
+library(tidyr)
+library(ggplot2)
+library(dplyr) # das hier immer als letztes du Nase
--- packages.R
+++ packages.R
@@ -21,25 +21,3 @@ install.packages("naniar")
 install.packages("ggridges")
 install.packages("GGally")
 install.packages("rio") #loading data
-
-
-library(rio) #reading data into R
-library(foreign) 
-library(car) # scatter3d()
-library(Hmisc) # describe(mydata)
-library(varhandle)
-library(pastecs) #stat.desc(mydata)
-library(rms)
-library(survminer)
-library(plm)
-library(data.table)
-library(bigmemory)
-library(Matrix)
-library(stats)
-library(survival)
-library(bitops)
-library(haven)
-library(plyr) #plyr muss vor dplyr
-library(tidyr)
-library(ggplot2)
-library(dplyr) # das hier immer als letztes du Nase
