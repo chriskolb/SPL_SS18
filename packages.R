ecba38c65c2e71bb0c42adb908f96dfed7d08f48
konstantingoebler <goeblerk@hu-berlin.de>
2018-06-11 14:38:44 +0200

    not sure

--- packages.R
+++ packages.R
@@ -1,19 +1,30 @@
-
+#install packages
 install.packages("bigmemory",dependencies=TRUE)
 install.packages("biganalytics",dependencies=TRUE)
 install.packages("dplyr",dependencies=TRUE)
 install.packages("plm")
 install.packages("varhandle")
 install.packages("survival")
-install.packages("haven")
-install.packages("tidyr")
-install.packages("bitops")
+install.packages("rms")
 install.packages("survminer")
 install.packages("Hmisc")
 install.packages("pastecs")
 install.packages("rms")
 install.packages("car",dependencies=TRUE)
 install.packages("rio", dependencies = TRUE)
+install.packages("pastecs") #summary stats
+install.packages("Hmisc") #summary stats 
+install.packages(c("rgl", "car"))#scatter3d()
+install.packages("haven")
+install.packages("VIM")
+install.packages("naniar")
+install.packages("ggridges")
+install.packages("GGally")
+install.packages("rio") #loading data
+install.packages("tidyverse")
+install.packages("readr")
+
+
 
 library(rio) #reading data into R
 library(foreign) 
@@ -35,3 +46,4 @@ library(plyr) #plyr muss vor dplyr
 library(tidyr)
 library(ggplot2)
 library(dplyr) # das hier immer als letztes du Nase
+
