a0569aabacdd6b522bed1ec4b8fc201ea19135d6
chriskolb <39267431+chriskolb@users.noreply.github.com>
2018-08-07 20:49:47 +0200

    Update master.R
    correct order of source commands

--- master.R
+++ master.R
@@ -7,14 +7,14 @@ rm(list=ls())
 # load path to SOEP data
 source(".path.R")
 
-# Wrangle the data
-source("datawrangling.R")
-
 # install and load packages
 source("packages.R")
 
 # load all written functions 
 source("functions.R")
+
+# Wrangle the data
+source("datawrangling.R")
 
 # load descriptives 
 source("descriptives.R")
