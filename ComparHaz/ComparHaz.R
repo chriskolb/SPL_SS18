74fabddf62dc953147f6a162c3506b9bdeff9572
aliciiiii <39268116+aliciiiii@users.noreply.github.com>
2018-08-08 14:11:17 +0200

    Update ComparHaz.R

--- ComparHaz/ComparHaz.R
+++ ComparHaz/ComparHaz.R
@@ -1,3 +1,45 @@
+libraries = c("survival", "rms", "survminer", "dplyr", "readr", "flexsurv", "ggfortify", "ggplot2")
+lapply(libraries, function(x)if(!(x %in% installed.packages()))
+{install.packages(x)})
+lapply(libraries, library, quietly=TRUE, character.only=TRUE)
+
+#load dataset
+load("datfinal.RDA")
+
+# define formula
+coxsurv <- Surv(dat$time, dat$event, type="right")
+coxform <- as.formula("coxsurv ~ hhinc + rural + maxedu + region + migback + married + ever_div")
+dat <- within(dat,{
+  rural <- factor(rural, labels = c("urban", "rural"))
+  region <- factor(region, labels = c("west", "east"))
+  migback <- factor(migback, labels = c("No", "Yes"))
+  married <- factor(married, labels = c("No", "Yes"))
+  ever_div <- factor(ever_div, labels = c("No", "Yes"))
+})
+cox.ph <- coxph(coxform, data=dat)
+# define survival object
+coxparm <- Surv(dat$time, dat$event, type="right")
+
+# define model formula
+parmform <- as.formula("coxparm ~ hhinc + rural + maxedu + region + migback + married + ever_div")
+# Kaplan-Meier estimator
+kapm <- survfit(coxparm ~ 1, data=dat)
+# puts survival table from kapm object into a data frame
+kap.dat <- fortify(kapm)
+# fortify cox model output
+cox.dat <- fortify(survfit(cox.ph, conf.int = F))
+
+# Exponential distribution
+expo <- flexsurvreg(formula = parmform, data = dat, dist = "exp")
+# Weibull distribution
+weibull <- flexsurvreg(formula = parmform, data = dat, dist = "weibull")
+# Log-Logistic distribution
+loglog <- flexsurvreg(formula = parmform, data = dat, dist = "llogis")
+# Log-normal distribution
+lnormal <- flexsurvreg(formula = parmform, data = dat, dist = "lnorm")
+# Flexible splines (Royston and Parmar 2002)
+flex.spline <- flexsurvspline(coxparm ~ 1, data = dat, k = 2, scale = "odds")
+
 # only hazard functions as single plot
 ggplot(data.frame(summary(expo, type = "hazard")), aes(x = time)) + 
   geom_line(aes(y = est, col = "Exponential")) +
