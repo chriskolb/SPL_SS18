a3407a5256633e7fbdf687d9fd93d498d6e79400
chriskolb <39267431+chriskolb@users.noreply.github.com>
2018-08-08 17:03:18 +0200

    Update analysis.R
    clean up code

--- analysis.R
+++ analysis.R
@@ -1,34 +1,46 @@
 
-##################################################################################
-##################################################################################
-#################### ANALYSIS ####################################################
-##################################################################################
-##################################################################################
+##########################################################################################
+##########################################################################################
+#################### ANALYSIS ############################################################
+##########################################################################################
+##########################################################################################
 
 
-# Things to do:
+#Note1: All scripts and SOEP data files need to be in the same directory
+#Note2: .path.R file needs to be specified by the user
+#Note3: This script needs to load functions.R script first
 
-# Classic nonparametric estimators:
-# CDF, Survival function, cumulative hazard function for KM and Nelson/Aalen/Fleming-Harrington (done)
-# KM by strata (most interesting ones migback, highinc etc.) (done)
+#Structure:
+#0: Set-up
+#1: Comparison of KM/FH estimators
+#2: KM by strata
+#3: Cox PH model
+#4: Cox PH model diagnostics
+#5: Comparison plots (survival curve, hazard function)
+#6: Comparison tables (estimated coefficients)
 
-# Cox PH regression:
-# results table, plot overall, ggadjust plots, schoenfeld test plot, hazard ratios, other
 
-# Comparison Parametric Models and Cox PH / KM:
-# Coefficient table for different distributions and Cox PH in comparison, AICs table (done)
-# Plot comparing Cox PH, KM, and parametric models (done)
 
-# Mordern nonparametric estimator: random survival forest:
-# survival function plots, plots for different strata, concordance index between models,
-# prediction error curve, partial dependence surface, VIMP (variable importance)
-# ggRandomForests
+##########################################################################################
+#### Set Up ##############################################################################
+##########################################################################################
+
+
+#clear workspace
+rm(list=ls())
+
+#setwd(path) in path.R
+source(".path.R")
+source("functions.R")
+
+#install and load packages
+source("packages.R")
 
 load("datfinal.RDA")
 
-##################################################################################################
-# Comparison Kaplan-Meier & Nelson-Aalen/Fleming-Harrington ######################################
-##################################################################################################
+##########################################################################################
+#### Comparison Kaplan-Meier & Nelson-Aalen/Fleming-Harrington ###########################
+##########################################################################################
 
 # Kaplan-Meier estimator
 km.fit <- survfit(Surv(time,event, type="right") ~ 1, data=dat, type="kaplan-meier")
@@ -44,99 +56,83 @@ surv.all <- nonparametricKurves()
 cumhaz.all <- nonparametricKurves("cumhaz")
 
 # put all plots in one graph
-
 kmfh.glist <- list(surv.all, cumhaz.all)
-
 arrange_ggsurvplots(kmfh.glist, print = TRUE, ncol = 2, nrow = 1)
 
-##################################################################################################
-# KM by strata ###################################################################################
-##################################################################################################
+##########################################################################################
+#### KM by strata ########################################################################
+##########################################################################################
 
-# KM by gender ###################################################################################
+#### KM by gender ###
 # 0 = male, 1 = female
 wide.fit <- survfit(Surv(time, event, type="right") ~ gender, data=dat)
-
 km.sex <- kmGroupKurves(c("Male", "Female"), "Gender")
 
 rm(wide.fit)
 
-# KM by Metropolitan Area ###################################################################################
+### KM by Metropolitan Area ###
 # 0 = Urban, 1 = Rural
 wide.fit <- survfit(Surv(time, event, type="right") ~ rural, data=dat)
-
 km.urban <- kmGroupKurves(c("Urban", "Rural"), "Metropolitan Area")
 
 rm(wide.fit)
 
-# KM by married divorced ###################################################################################
-
-wide.fit <- survfit(Surv(time, event, type="right") ~ ever_div, data=dat)
-
-km.div <- kmGroupKurves(c("No", "Yes"), "Ever Divorced")
-
-
+#### KM by married ###
 wide.fit <- survfit(Surv(time, event, type="right") ~ married, data=dat)
-
 km.marr <- kmGroupKurves(c("No", "Yes"), "Married")
-
 
 rm(wide.fit)
 
-# KM by region ###################################################################################
+#### KM by ever_divorced ###
+wide.fit <- survfit(Surv(time, event, type="right") ~ ever_div, data=dat)
+km.div <- kmGroupKurves(c("No", "Yes"), "Ever Divorced")
 
+rm(wide.fit)
+
+#### KM by region ####
 wide.fit <- survfit(Surv(time, event, type="right") ~ region, data=dat)
-
 km.reg <- kmGroupKurves(c("West", "East"), "Region")
 
 rm(wide.fit)
 
 
-# KM by migback ####################################################################
-
+#### KM by migback ####
 wide.fit <- survfit(Surv(time, event, type="right") ~ migback, data=dat)
-
 km.mig <- kmGroupKurves(c("No", "Yes"), "Migr.Back.")
 
 rm(wide.fit)
 
 
-# KM by highinc/lowinc ###########################################################
-
+#### KM by highinc/lowinc ###
+#define highinc variable as above median household income
 medinc <- median(as.numeric(dat$hhinc), na.rm=TRUE)
 dat.inc <- mutate(dat, highinc = ifelse(dat$hhinc > medinc, 1, 0))
 summary(dat.inc$highinc)
 #define survival object and fit KM estimator
 wide.fit <- survfit(Surv(time, event, type="right") ~ highinc, data=dat.inc)
-
 km.inc <- kmGroupKurves(c("Low", "High"),"HH Inc.")
 
 rm(wide.fit, medinc, dat.inc)
 
 
-# KM by educ (ISCED 97) ###########################################################
-
-#define survival object and fit KM estimator
+#### KM by educ (ISCED 97) ###
 wide.fit <- survfit(Surv(time, event, type="right") ~ educ, data=dat)
-
 km.edu <- kmGroupKurves(c("Elementary", "Medium", "Higher voc.", "High"),"Education", line = c(1,1,1,1), conf=F)
 
 rm(wide.fit)
 
-# KM by cohorts 84-87 and 94-97 #########################################################
-
+### KM by cohorts 84-87 and 94-97 ###
 dat <- mutate(dat, cohort8494 = ifelse (dat$firstyear<=1987, 1,ifelse(dat$firstyear>=1994 & dat$firstyear<=1997, 2, NA)))
 summary(dat$cohort8494)
 table(dat$cohort8494)
 #define survival object and fit KM estimator
 wide.fit <- survfit(Surv(time, event, type="right") ~ cohort8494, data=dat)
-
 km.coh <- kmGroupKurves(c("84-87", "94-97"),"Cohorts")
 
 rm(wide.fit)
 
 
-# arrange plots ############################################################
+### generate arranged plots ###
 
 km.glist1 <- list(km.inc, km.mig)
 
@@ -156,7 +152,7 @@ km.plot3 <- arrange_ggsurvplots(km.glist3, ncol = 2, nrow = 1, print = FALSE,
                                 risk.table.height = 0.25,
                                 surv.plot.height = 1)
 
-# print KM by strata plots ##################################################
+#### print 'KM by strata' plots ###
 
 print(km.plot1)
 print(km.plot2)
@@ -164,28 +160,24 @@ print(km.plot3)
 print(km.edu)
 
 
-##################################################################################################
-# Cox Proportional Hazards Regression ############################################################
-##################################################################################################
+##########################################################################################
+### Cox Proportional Hazards Regression ##################################################
+##########################################################################################
 
-#survival package to estimate models, survminer package for plots and diagnostics
+# survival package to estimate models, survminer package for plots and diagnostics
+
 # define survival object
-
 coxsurv <- Surv(dat$time, dat$event, type="right")
 
 # define formula
-
 coxform <- as.formula("coxsurv ~ hhinc + rural + maxedu + region + migback + married + ever_div")
 
 # estimate Cox regression
-
 cox.ph <- coxph(coxform, data=dat)
 summary(cox.ph)
 
 # Cox PH model table
-
 stargazer(cox.ph)
-
 displayCoxPH(cox.ph, cap = "", dig.coef = 3, dig.p = 2)
 
 # table in overleaf is constructed from both outputs
@@ -204,52 +196,39 @@ cox.ph <- coxph(coxform, data=dat)
 ggforest(cox.ph)
 
 
-# adjusted survival curves from cox model
-# not needed later
-# by highinc
-medinc <- median(dat$hhinc, na.rm=TRUE)
-dat <- mutate(dat, highinc = ifelse(dat$hhinc > medinc, 2, 1))
-summary(dat$highinc)
-rm(medinc)
-
-ggadjustedcurves(cox.ph, data = dat, variable = "highinc")
-
-# by education level (ISCED97)
-
-ggadjustedcurves(cox.ph, data = dat, variable = "educ")
+##########################################################################################
+### Cox Model Diagnostics ################################################################
+##########################################################################################
 
 
-##### Model Diagnostics #####
-### (Schoenfeld test, influential observations) ###
+### testing proportional hazards assumption (Schoenfeld) ###
 
-
-# Schoenfeld test of cox ph assumption
-
+# Schoenfeld test
 coxtest <- cox.zph(cox.ph, transform = "km")
 coxtest
 stargazer(coxtest$table, out = "schoenfeld.tex")
 
-# Schoenfeld graphical test of cox ph assumption
+# Schoenfeld graphical test of cox ph assumption #
 
-# scaled Schoenfeld plots for only selected variables (have to be continuous)
+# scaled Schoenfeld plots for two selected variables
 ggcoxzph(coxtest, resid = T, point.col="lightcoral", point.alpha = 0.4, var=c("hhinc","maxedu"))
 
+# scaled Schoenfeld plots for all variables
 ggcoxzph(coxtest, point.alpha = 0.4, point.col="lightcoral")
 
 
-###--------- Testing for influential Observations ----------####
+### testing for influential Observations ####
 
 # use Delta-Beta residuals to detect influential observations scaled by standard errors of coefficients
-
 ggcoxdiagnostics(cox.ph, type = "dfbetas", ox.scale= "observation.id", hline.col = "darkgreen", hline.alpha = 0.5, point.alpha = 0.4, point.col = "lightcoral", sline.alpha = 0.4, sline.col = "dodgerblue")
 
 
 rm(cox.ph, coxtest, coxsurv, coxform)
 
 
-##################################################################################################
-# KM, Cox PH and Parametric distributions plot ###################################################
-##################################################################################################
+##########################################################################################
+#### KM, Cox PH and Parametric distributions plot ########################################
+##########################################################################################
 
 # define survival object
 coxparm <- Surv(dat$time, dat$event, type="right")
@@ -260,10 +239,10 @@ parmform <- as.formula("coxparm ~ hhinc + rural + maxedu + region + migback + ma
 # Kaplan-Meier estimator
 kapm <- survfit(coxparm ~ 1, data=dat)
 
-# fortify as well as tidy from the broom package
+# fortify 
 # puts survival table from kapm object into a data frame
 kap.dat <- fortify(kapm)
-#kap.dat2 <- tidy(kapm)
+
 
 # Cox PH model
 cox.ph <- coxph(formula=parmform, data=dat)
@@ -287,11 +266,10 @@ loglog <- flexsurvreg(formula = parmform, data = dat, dist = "llogis")
 # Log-normal distribution
 lnormal <- flexsurvreg(formula = parmform, data = dat, dist = "lnorm")
 
+ 
+### plot all curves together ###
 
-# plot all curves together
-
-# graph approach 1 (from rpubs paper)
-# plot for Cox PH model is (by default) at average of covariates
+# Note: plot for Cox PH model is (by default) at average of covariates
 grid.arrange(
   ggplot(data.frame(summary(expo)), aes(x = time)) + 
     geom_line(aes(y = est, col = "Exponential")) +
@@ -334,9 +312,9 @@ ggplot(data.frame(summary(expo, type = "hazard")), aes(x = time)) +
 
 
 
-##################################################################################################
-# Cox PH and Parametric distributions tables #####################################################
-##################################################################################################
+##########################################################################################
+#### Cox PH and Parametric distributions tables ##########################################
+##########################################################################################
 
 # stargazer only compatible with survreg, not flexsurvreg
 
@@ -385,31 +363,8 @@ AICs[4, 2] <- lnormal$loglik
 rownames(AICs) <- c("Weibull", "Exponential", "Log-Logistic", "Log-Normal")
 colnames(AICs) <- c("AIC", "Log-Likelihood")
 t(AICs)
+
 #Log-normal has best fit
 stargazer(t(AICs), out="aic.tex")
 
-#### plot log-normal cumulative hazard against KM ####
-
-kmcumhaz <- ggsurvplot(kapm, fun = "cumhaz")
-# Log-normal distribution
-lnormal <- flexsurvreg(coxparm~1, data = dat, dist = "lnorm")
-lncumhaz <- ggflexsurvplot(lnormal, fun="cumhaz")
-
-
-# Random Survival Forests #################################################
-#preliminary version
-
-fitform <- Surv(time, event) ~ hhinc + rural + maxedu + region + migback
-
-set.seed(0692)
-rsf <- rfsrc(fitform, data = dat, forest = TRUE, ntree = 500, importance = TRUE)
-print(rsf)
-
-
-#ggRFsrc <- plot(gg_rfsrc(rsf), alpha = 0.2)
-#show(ggRFsrc)
-
-plot(gg_rfsrc(rsf, by = "region"))
-
-plot(gg_rfsrc(rsf, by = "migback"))
 
