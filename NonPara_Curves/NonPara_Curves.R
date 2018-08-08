9f001e3a3abc66c60bb39d2bc21b321e49793bc5
aliciiiii <39268116+aliciiiii@users.noreply.github.com>
2018-08-08 13:21:19 +0200

    Update NonPara_Curves.R

--- NonPara_Curves/NonPara_Curves.R
+++ NonPara_Curves/NonPara_Curves.R
@@ -1,12 +1,16 @@
 rm(list=ls())
 
-source(".path.R")
+# set working directory setwd('C:/...') 
+# setwd('~/...')    # linux/mac os
+# setwd('/Users/...') # windows
+
 #install and load packages
 libraries = c("survival", "rms", "survminer", "dplyr", "readr", "ggplot2")
 lapply(libraries, function(x)if(!(x %in% installed.packages()))
    {install.packages(x)})
 lapply(libraries, library, quietly=TRUE, character.only=TRUE)
 
+#load dataset
 load("datfinal.RDA")
 
 #### Function for Kaplan Meier Curves by Strata ####
