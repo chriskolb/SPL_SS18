1a3f0d4b26324ab6286241b25ec64699f010b337
aliciiiii <39268116+aliciiiii@users.noreply.github.com>
2018-08-08 13:43:45 +0200

    Update GroupKM.R

--- GroupKM/GroupKM.R
+++ GroupKM/GroupKM.R
@@ -1,4 +1,18 @@
-tidy_source(file="GroupKM.R", width.cutoff = 80, indent = 4)
+rm(list=ls())
+
+# set working directory setwd('C:/...') 
+# setwd('~/...')    # linux/mac os
+# setwd('/Users/...') # windows
+
+#install and load packages
+libraries = c("")
+lapply(libraries, function(x)if(!(x %in% installed.packages()))
+   {install.packages(x)})
+lapply(libraries, library, quietly=TRUE, character.only=TRUE)
+
+#load dataset
+load("datfinal.RDA")
+
 ##################################################################################################
 # KM by strata ###################################################################################
 ##################################################################################################
