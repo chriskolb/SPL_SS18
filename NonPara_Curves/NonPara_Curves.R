70fbf59b48741df125a1a4e866c5737ff22a15a8
aliciiiii <39268116+aliciiiii@users.noreply.github.com>
2018-08-09 22:31:47 +0200

    format

--- NonPara_Curves/NonPara_Curves.R
+++ NonPara_Curves/NonPara_Curves.R
@@ -1,14 +1,15 @@
 
 rm(list = ls())
 
-# set working directory setwd('C:/...') setwd('~/...') # linux/mac os
+# set working directory setwd('C:/...') 
+# setwd('~/...') # linux/mac os
 # setwd('/Users/...') # windows
 
 
 # install and load packages
 libraries = c("survival", "rms", "survminer", "dplyr", "readr", "ggplot2")
 lapply(libraries, function(x) if (!(x %in% installed.packages())) {
-    install.packages(x)
+  install.packages(x)
 })
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
@@ -25,19 +26,18 @@ km.fit = survfit(Surv(time, event, type = "right") ~ 1, data = dat,
                  type = "kaplan-meier")
 
 # Fleming-Harrington estimator
-fh.fit = survfit(Surv(time, event, type = "right") ~ 1, data = dat, 
+fh.fit = survfit(Surv(time, event, type = "right") ~ 1, data = dat,
                  type = "fleming-harrington")
 kmfh.all = list(km.fit, fh.fit)
 
 #### Function for ggsurvplot_combine ##### store survplot object and choose
 #### functional argument (default is Survival Function)
-nonparametricKurves = function(fun = NULL) {
-    ggsurvplot_combine(kmfh.all, data = dat, conf.int = T, 
-                       legend.labs = c("KM", "Fleming-Harrington"), 
-                       legend.title = "Model", fun = fun, risk.table = F, 
-        cumcensor = FALSE, censor = FALSE, linetype = c(1, 1), size = 0.3)
-}
 
+nonparametricKurves = function(fun = NULL) {
+  ggsurvplot_combine(kmfh.all, data = dat, conf.int = T, legend.labs = c("KM", 
+  "Fleming-Harrington"), legend.title = "Model", fun = fun, risk.table = F, 
+  cumcensor = FALSE, censor = FALSE, linetype = c(1, 1), size = 0.3)
+}
 
 
 # Survival Function
