3e399eb80b05a51a58f15076d187f4133607913c
chriskolb <39267431+chriskolb@users.noreply.github.com>
2018-08-10 11:36:13 +0200

    Update master.R
    change order

--- master.R
+++ master.R
@@ -16,12 +16,12 @@ source("path.R")
 # Install and load packages (runtime <1sec)
 source("packages.R")
 
-# Load written functions (runtime <1sec)
-source("functions.R")
-
 # Wrangle the data (runtime 337sec)
 # This script produces datfinal.RDA from SOEP .csv files
 source("datawrangling.R")
+
+# Load written functions (runtime <1sec)
+source("functions.R")
 
 # Load descriptives (runtime 11sec)
 source("descriptives.R")
